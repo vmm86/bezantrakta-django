7d50e80dcfa4fac97a27e8ba66e41e126b640533
vmm <vmm86@users.noreply.github.com>
2017-05-22 11:46:31 +0000

    Исходные общие для всего проекта папки для статики и медиафайлов

---
+++ bezantrakta/bezantrakta_project/static/js/bxslider/carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    mode: 'vertical',
+    controls: false,
+    touchEnabled: true,
+    auto: true,
+    randomStart: true,
+    speed: 2000,
+    slideWidth: 822,
+    minSlides: 1,
+    slideMargin: 0
+  });
+});
