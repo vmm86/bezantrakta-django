df459c137a44587551846b9724741e0b9d58ab71
vmm <vmm86@users.noreply.github.com>
2017-05-26 08:29:58 +0000

    Корректная настройка кастомных приложений Безантракта и доп. опции в настройках сайта

--- bezantrakta/article/admin.py
+++ bezantrakta/article/admin.py
@@ -8,6 +8,12 @@ class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',),
     }
-    list_display = ('title', 'is_published', 'domain',)
+    list_display = ('title', 'slug', 'is_published', 'domain',)
+    # list_editable = ('is_published',)
     list_filter = ('domain',)
     # radio_fields = {'domain': admin.VERTICAL}
+
+    # def view_domain(self, obj):
+    #     return obj.domain
+
+    # view_domain.empty_value_display = 'любой домен'
--- bezantrakta/article/apps.py
+++ bezantrakta/article/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+# from django.utils.translation import ugettext_lazy as _
 
 
 class ArticleConfig(AppConfig):
-    name = 'article'
-    verbose_name = 'Страницы'
+    name = 'bezantrakta.article'
+    verbose_name = 'HTML-страницы'
+    # verbose_name = _('article')
