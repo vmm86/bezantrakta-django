49f957b5ecbe0a41a7357b8e20e41191a7c713ab
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-05-29 14:09:55 +0000

    Обслуживание медиафайлов вместе со статикой в режиме разработки

--- trunk/project/urls.py
+++ trunk/project/urls.py
@@ -39,6 +39,11 @@ if settings.DEBUG:
     ] + urlpatterns
 
     urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+    urlpatterns += static(
         settings.STATIC_URL,
         document_root=settings.STATIC_ROOT
     )
