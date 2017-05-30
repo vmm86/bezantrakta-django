4a72d79943ed7c3ec3cd952d1ab47cc1ceef05ba
vmm <vmm86@users.noreply.github.com>
2017-05-30 10:10:06 +0000

    Перевод логики отображения баннеров из шаблона в context processor

--- bezantrakta/banner/context_processors.py
+++ bezantrakta/banner/context_processors.py
@@ -14,9 +14,10 @@ def banner_group_items(request):
     banner_group_items = {}
     for slug, title in banner_group.items():
         banner_group_items[slug] = BannerGroupItem.objects.filter(
+            is_published=True,
             banner_group__slug=slug,
             domain__slug=request.domain,
-        ).values('title', 'img', 'href', 'is_published')
+        ).values('title', 'img', 'href')
 
     return {
         'banner_group': banner_group,
