6d399e4a7fd83dfe3dc6c8848ad13e170e9b5d41
vmm <vmm86@users.noreply.github.com>
2017-06-11 18:01:22 +0000

    Настройки проекта перенесены в пакет settings и разбиты на разные модули

--- project/settings/base.py
+++ project/settings/base.py
@@ -1,7 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 INTERNAL_IPS = ['127.0.0.1']
 
