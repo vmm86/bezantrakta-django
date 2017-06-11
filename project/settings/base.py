6b665632d8afa0f150ba072a47655945b5ba8f1a
vmm <vmm86@users.noreply.github.com>
2017-06-11 20:23:11 +0000

    Базовые настройки языка

--- project/settings/base.py
+++ project/settings/base.py
@@ -213,6 +213,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+LANGUAGES = [
+    ('ru', 'Русский')
+]
+
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
@@ -221,7 +225,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-# LOCALE_PATHS = (os.path.join(BASE_DIR, 'project', 'locale'),)
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'project', 'locale'),)
 
 USE_TZ = True
 
