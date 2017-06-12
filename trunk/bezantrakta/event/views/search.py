89b543a40428876f5aecd777a35ddd6abfacf8cb
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-06-12 10:29:11 +0000

    Возможность поиска по полям title, description, keywords модели event

---
+++ trunk/bezantrakta/event/views/search.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.db.models import F, Q
+from django.shortcuts import render
+
+from project.shortcuts import add_small_vertical_poster
+
+from ..models import Event
+
+
+def search(request):
+    try:
+        text = request.GET['text']
+    except KeyError:
+        pass
+    else:
+        today = datetime.datetime.today()
+
+        events = Event.objects.select_related(
+            'event_venue',
+            'domain'
+        ).annotate(
+            venue=F('event_venue__title'),
+        ).filter(
+            Q(title__icontains=text) | Q(description__icontains=text) | Q(keywords__icontains=text),
+            is_published=True,
+            date__gt=today,
+            domain_id=request.domain_id
+        ).values(
+            'title',
+            'slug',
+            'date',
+            'time',
+            'min_price',
+            'min_age',
+            'venue',
+        )
+
+        add_small_vertical_poster(request, events)
+
+        context = {'events': events}
+
+        return render(request, 'event/search.html', context)
