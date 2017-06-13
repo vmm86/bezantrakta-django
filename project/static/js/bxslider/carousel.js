27e34e18bee410c9b001d3e6d7bfa8fcdfed8477
vmm <vmm86@users.noreply.github.com>
2017-06-13 20:58:58 +0000

    Коррективы настроек карусели

--- project/static/js/bxslider/carousel.js
+++ project/static/js/bxslider/carousel.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
     infiniteLoop: true,
     auto: true,
     speed: 1500,
-    slideWidth: 822,
+    // slideWidth: 822,
     minSlides: 1,
     slideMargin: 0,
     responsive: true,
-    preloadImages: 'visible',
+    preloadImages: 'visible'
     // controls: true
     });
 });
