b08a4bf80f7997e8a5e22b12aac93e77ff9ceb56
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-06-16 08:37:38 +0000

    Обходное решение для скрытия вываливающихся вниз изображений в карусели при её загрузке

--- trunk/project/static/js/bxslider/carousel.js
+++ trunk/project/static/js/bxslider/carousel.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
-  $('.slider').bxSlider({
-    mode: 'vertical',
-    controls: false,
-    touchEnabled: true,
-    auto: true,
-    // randomStart: true,
-    infiniteLoop: true,
-    auto: true,
-    speed: 1500,
-    // slideWidth: 822,
-    minSlides: 1,
-    slideMargin: 0,
-    responsive: true,
-    preloadImages: 'visible'
-    // controls: true
+    $('.slider').bxSlider({
+        mode: 'vertical',
+        controls: false,
+        touchEnabled: true,
+        auto: true,
+        // randomStart: true,
+        infiniteLoop: true,
+        auto: true,
+        speed: 1500,
+        slideWidth: 822,
+        minSlides: 1,
+        slideMargin: 0,
+        responsive: true,
+        preloadImages: 'visible',
+        // controls: true,
+        onSliderLoad: function() {
+            $('.posters-level1-body').css('opacity', '1');
+            $('.posters-level1-body').css('height', 'auto');
+        }
     });
 });
