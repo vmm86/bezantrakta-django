755dff029b2670336761454e67af9bd88c2cb391
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-06-20 09:39:05 +0000

    Обновление тестовых данных приложения location

--- trunk/bezantrakta/location/bezantrakta_location_city.sql
+++ trunk/bezantrakta/location/bezantrakta_location_city.sql
@@ -1,68 +1,49 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6
--- https://www.phpmyadmin.net/
---
--- Хост: localhost
--- Время создания: Июн 13 2017 г., 10:30
--- Версия сервера: 5.7.18-0ubuntu0.16.04.1
--- Версия PHP: 7.0.18-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `belcanto_bezantrakta_django`
---
-
---
--- Дамп данных таблицы `bezantrakta_location_city`
---
-
-INSERT INTO `bezantrakta_location_city` (`id`, `title`, `slug`, `timezone`, `is_published`) VALUES
-(391, 'Красноярск', 'ksn', 'Asia/Krasnoyarsk', 0),
+INSERT INTO `bezantrakta_location_city` (`id`, `title`, `slug`, `timezone`, `state`) VALUES
+(383, 'Новосибирск', 'nsk', 'Asia/Novosibirsk', 0),
+(391, 'Красноярск', 'krsk', 'Asia/Krasnoyarsk', 0),
+(423, 'Владивосток', 'vl', 'Asia/Vladivostok', 0),
 (473, 'Воронеж', 'vrn', 'Europe/Moscow', 1),
-(831, 'Нижний Новгород', 'nnov', 'Europe/Moscow', 0),
+(484, 'Обнинск', 'obn', 'Europe/Moscow', 0),
+(831, 'Нижний Новгород', 'nnov', 'Europe/Moscow', NULL),
 (846, 'Самара', 'sam', 'Europe/Samara', 0),
-(861, 'Краснодар', 'kdr', 'Europe/Moscow', 0),
-(863, 'Ростов-на-Дону', 'rnd', 'Europe/Moscow', 0),
-(3462, 'Сургут', 'sur', 'Asia/Yekaterinburg', 0),
+(861, 'Краснодар', 'krd', 'Europe/Moscow', 1),
+(863, 'Ростов-на-Дону', 'rnd', 'Europe/Moscow', NULL),
+(3022, 'Чита', 'chita', 'Asia/Yakutsk', 0),
+(3452, 'Тюмень', 'tyu', 'Asia/Yekaterinburg', 0),
+(3462, 'Сургут', 'sur', 'Asia/Yekaterinburg', NULL),
 (3466, 'Нижневартовск', 'nvar', 'Asia/Yekaterinburg', 0),
-(3532, 'Оренбург', 'ore', 'Asia/Yekaterinburg', 0),
+(3532, 'Оренбург', 'oren', 'Asia/Yekaterinburg', 0),
 (3812, 'Омск', 'omsk', 'Asia/Omsk', 0),
 (3952, 'Иркутск', 'irk', 'Asia/Irkutsk', 0),
-(4712, 'Курск', 'kur', 'Europe/Moscow', 0),
+(4012, 'Калининград', 'kgd', 'Europe/Kaliningrad', 0),
+(4162, 'Благовещенск', 'blag', 'Asia/Yakutsk', 0),
+(4212, 'Хабаровск', 'hab', 'Asia/Vladivostok', 0),
+(4234, 'Уссурийск', 'ussur', 'Asia/Vladivostok', 0),
+(4242, 'Южно-Сахалинск', 'ysah', 'Asia/Sakhalin', 0),
+(4712, 'Курск', 'kur', 'Europe/Moscow', 1),
 (4722, 'Белгород', 'bel', 'Europe/Moscow', 1),
-(4725, 'Старый Оскол', 'sosk', 'Europe/Moscow', 0),
-(4742, 'Липецк', 'lip', 'Europe/Moscow', 0),
-(4752, 'Тамбов', 'tam', 'Europe/Moscow', 0),
-(4812, 'Смоленск', 'smo', 'Europe/Moscow', 0),
-(4822, 'Тверь', 'tve', 'Europe/Moscow', 0),
+(4725, 'Старый Оскол', 'osk', 'Europe/Moscow', 0),
+(4742, 'Липецк', 'lip', 'Europe/Moscow', 1),
+(4752, 'Тамбов', 'tam', 'Europe/Moscow', NULL),
+(4812, 'Смоленск', 'smol', 'Europe/Moscow', 0),
+(4822, 'Тверь', 'tver', 'Europe/Moscow', 0),
 (4832, 'Брянск', 'bry', 'Europe/Moscow', 0),
 (4842, 'Калуга', 'kal', 'Europe/Moscow', 0),
 (4852, 'Ярославль', 'yar', 'Europe/Moscow', 0),
-(4862, 'Орел', 'orel', 'Europe/Moscow', 0),
+(4862, 'Орел', 'orel', 'Europe/Moscow', 1),
 (4872, 'Тула', 'tula', 'Europe/Moscow', 0),
 (4912, 'Рязань', 'rzn', 'Europe/Moscow', 0),
-(4922, 'Владимир', 'vdm', 'Europe/Moscow', 0),
-(4932, 'Иваново', 'iva', 'Europe/Moscow', 0),
-(4942, 'Кострома', 'ktr', 'Europe/Moscow', 0),
-(8112, 'Псков', 'pskov', 'Europe/Moscow', 0),
+(4922, 'Владимир', 'vlad', 'Europe/Moscow', 0),
+(4932, 'Иваново', 'ivan', 'Europe/Moscow', 0),
+(4942, 'Кострома', 'kos', 'Europe/Moscow', 0),
+(8112, 'Псков', 'psk', 'Europe/Moscow', 0),
 (8162, 'Великий Новгород', 'vnov', 'Europe/Moscow', 0),
-(8182, 'Архангельск', 'akh', 'Europe/Moscow', 0),
-(8412, 'Пенза', 'penza', 'Europe/Moscow', 0),
+(8172, 'Вологда', 'vo', 'Europe/Moscow', 0),
+(8182, 'Архангельск', 'arh', 'Europe/Moscow', NULL),
+(8412, 'Пенза', 'pnz', 'Europe/Moscow', 0),
 (8422, 'Ульяновск', 'uly', 'Europe/Moscow', 0),
 (8442, 'Волгоград', 'vol', 'Europe/Volgograd', 0),
 (8452, 'Саратов', 'sar', 'Europe/Volgograd', 1),
-(8482, 'Тольятти', 'tol', 'Europe/Samara', 0),
-(81153, 'Великие Луки', 'vluki', 'Europe/Moscow', 0),
-(81842, 'Северодвинск', 'sdvn', 'Europe/Moscow', 0);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(8482, 'Тольятти', 'tlt', 'Europe/Samara', 0),
+(81153, 'Великие Луки', 'vluki', 'Europe/Moscow', NULL),
+(81842, 'Северодвинск', 'sdvin', 'Europe/Moscow', 0);
