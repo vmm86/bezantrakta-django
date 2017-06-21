b1a809ef05efd9f6fe89dc96053f434b6c15ac43
vmm <vmm86@users.noreply.github.com>
2017-06-21 14:08:40 +0000

    Foolproof: вывод названия текущего рабочего окружения в админке

--- project/settings/stage.py
+++ project/settings/stage.py
@@ -6,24 +6,24 @@ from .base import *
 # * staging - проверка работы на тестовом окружении (DEBUG = False)
 # * production - готовое бизнес-приложение (DEBUG = False)
 ENVIRONMENT = {
-    'NAME': 'production',
-    'COLOR': '#00C853'
+    'NAME': 'staging',
+    'COLOR': '#FFAB00'
 }
 
 DEBUG = False
 
-SECRET_KEY = 't%#tk0-z%+)4)bz7t4$hd8uc*^3rd8nrsn93&$y$9!al!e$7h#'
+SECRET_KEY = 'ye)g4dae85*jqw-py(88@vp4wg51$dvd1tcxe6y2f6e(x%_x4%'
 
 # Корневой домен для всех сайтов
-ROOT_DOMAIN = 'bezantrakta.ru'
+ROOT_DOMAIN = 'bezantrakta.rterm.ru'
 # Псевдоним сайта, работающего на корневом домене (указан в БД в модели Domain)
 ROOT_DOMAIN_SLUG = 'vrn'
 
 ALLOWED_HOSTS = [
-    '.bezantrakta.ru'
+    '.bezantrakta.rterm.ru'
 ]
 
 # Email для автоматических сообщений от менеджера сайта.
-DEFAULT_FROM_EMAIL = 'webmaster@bezantrakta.ru'
+DEFAULT_FROM_EMAIL = 'webmaster@rterm.ru'
 # The email that error messages come from, sent to ADMINS and MANAGERS.
-SERVER_EMAIL = 'webmaster@bezantrakta.ru'
+SERVER_EMAIL = 'webmaster@rterm.ru'
