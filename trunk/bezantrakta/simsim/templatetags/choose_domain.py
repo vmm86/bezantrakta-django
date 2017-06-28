76195ba251ba016aae67a74a4d1919e35cb46b80
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-06-28 10:21:42 +0000

    Выбор текущего города для работы в админке и активация при этом его часового пояса для локализации отображения даты/времени

--- trunk/bezantrakta/simsim/templatetags/choose_domain.py
+++ trunk/bezantrakta/simsim/templatetags/choose_domain.py
@@ -1,5 +1,5 @@
 from django import template
-from django.db.models import F, Q
+from django.db.models import F
 
 from bezantrakta.location.models import Domain
 
@@ -9,14 +9,16 @@ register = template.Library()
 @register.inclusion_tag('admin/choose_domain.html', takes_context=True)
 def choose_domain(context):
     # Список всех сайтов, город которых либо в процессе подготовки, либо включён
-    domains = Domain.objects.filter(
-        Q(city__state__isnull=True) | Q(city__state=True),
-    ).annotate(
+    domains = Domain.objects.select_related(
+        'city',
+    ).all().annotate(
+        state=F('city__state'),
         timezone=F('city__timezone'),
     ).values(
         'title',
         'slug',
         'is_published',
+        'state',
         'timezone',
     )
     return {
