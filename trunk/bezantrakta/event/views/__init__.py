c5e5a216a3f6ee10f46fcd2324c9d54a53447b3b
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2017-07-03 21:38:56 +0000

    Все события и группы событий хранятся в модели Event, а события какой-либо группы - в дочерней M2M-модели EventGroupBinder

--- trunk/bezantrakta/event/views/__init__.py
+++ trunk/bezantrakta/event/views/__init__.py
@@ -1,4 +1,4 @@
-from .venue_autocomplete import EventVenueAutocomplete
+# from .venue_autocomplete import EventVenueAutocomplete
 
 from .events_on_index import events_on_index
 from .category import category
