5b5229ba26889557a11087e0a256d079ec0ddf3c
vmm <vmm86@users.noreply.github.com>
2017-07-13 09:39:20 +0000

    Вывод часовых поясов России с их оффсетами при создании городов и новая ссылка для проверки часовых поясов конкретного города

--- bezantrakta/location/forms.py
+++ bezantrakta/location/forms.py
@@ -4,22 +4,31 @@ from django import forms
 
 from dal import autocomplete
 
-from .models import City
+from .models import City, timezone_offset_humanized
 
 
-def get_timezone_list():
-    return [tz for tz in pytz.country_timezones('ru')]
+def timezones_with_offsets(timezones):
+    offsets = ''
+    for tz in timezones:
+        offsets += '<br>{offset} {timezone}'.format(
+            offset=timezone_offset_humanized(pytz.timezone(tz)),
+            timezone=tz
+        )
+    return offsets
 
 
 class CityForm(forms.ModelForm):
+    timezone_list = [tz for tz in pytz.country_timezones('ru')]
     timezone = autocomplete.Select2ListChoiceField(
-        choice_list=get_timezone_list,
+        choice_list=timezone_list,
         # initial='Europe/Moscow',
         widget=autocomplete.ListSelect2(),
         label='Часовой пояс',
         help_text="""
-        <a href="https://askgeo.com/" target="_blank">👉 Поиск часового пояса для конкретного города</a>
-        """
+        <a href="http://www.worldtimezone.com/time-russia24ru.php" target="_blank">
+        👉 Поиск часового пояса для конкретного города</a>
+        <br>{timezones_with_offsets}
+        """.format(timezones_with_offsets=timezones_with_offsets(timezone_list))
     )
 
     class Meta:
