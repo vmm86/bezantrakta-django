5f4f81244b1825d7f4911a9010c5a8c8412b9e44
vmm <vmm86@users.noreply.github.com>
2017-11-13 14:10:56 +0000

    Возможность редактировать дату/время в группах в админ-панели

--- bezantrakta/event/admin/event.py
+++ bezantrakta/event/admin/event.py
@@ -119,7 +119,7 @@ class EventAdmin(admin.ModelAdmin):
         'event_category': admin.VERTICAL,
         'min_age': admin.HORIZONTAL,
     }
-    readonly_fields = ('ticket_service', 'ticket_service_event', 'ticket_service_scheme', 'ticket_service_prices',)
+    # readonly_fields = ('ticket_service', 'ticket_service_event', 'ticket_service_scheme', 'ticket_service_prices',)
     search_fields = ('title',)
 
     def get_view_on_site_url(self, obj=None):
@@ -156,9 +156,15 @@ class EventAdmin(admin.ModelAdmin):
         """В событиях из сервисов продажи билетов нельзя изменить информацию,
         приходящую непосредственно из сервиса продажи билетов.
         """
+        ro_fields = ['ticket_service', 'ticket_service_event',
+                     'ticket_service_scheme', 'ticket_service_prices',
+                     'is_group', 'event_venue', 'domain', ]
         if obj is not None and obj.ticket_service is not None:
-            return self.readonly_fields + ('datetime', 'is_group', 'event_venue', 'domain',)
-        return self.readonly_fields
+            # В группах оставлена возможность редактировать дату/время события,
+            # если она не обновится автоматически при запуске задания ``ts_discover``
+            if not obj.is_group:
+                ro_fields.append('datetime')
+        return ro_fields
 
     def has_delete_permission(self, request, obj=None):
         """Импортируемые из сервисов продажи билетов события нельзя удалить,
