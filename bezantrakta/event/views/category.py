bc0264be9480d33580cea6dd633e2b99e9dd5c81
vmm <vmm86@users.noreply.github.com>
2017-11-13 09:48:51 +0000

    При фильтрации событий по категориям у событий в группе можно не указывать категорию - она берётся из группы

--- bezantrakta/event/views/category.py
+++ bezantrakta/event/views/category.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.conf import settings
 from django.db.models import F, Q
 from django.shortcuts import render
@@ -16,30 +18,18 @@ def category(request, slug):
     """
     today = timezone_now()
 
-    # Получение событий во всех категориях или фильтр по конкретной категории
-    if slug == settings.BEZANTRAKTA_CATEGORY_ALL:
-        category_name = 'Все события'
-        category_event_filter = Q(event_category__isnull=False)
-    else:
-        category_name = EventCategory.objects.values_list('title', flat=True).get(slug=slug)
-        category_event_filter = Q(event_category__slug=slug)
-
     # Запрос событий из всех категорий или из какой-либо конкретной
     category_events = list(Event.objects.select_related(
         'event_category',
         'event_venue',
         'domain'
     ).annotate(
-        uuid=F('id'),
-        category_title=F('event_category__title'),
+        event_uuid=F('id'),
     ).values(
-        'uuid',
-        'category_title',
+        'event_uuid',
     ).filter(
         Q(is_group=False) &
         Q(is_published=True) &
-        Q(event_category__is_published=True) &
-        category_event_filter &
         Q(datetime__gt=today) &
         Q(domain_id=request.domain_id)
     ).order_by(
@@ -50,10 +40,18 @@ def category(request, slug):
     if category_events:
         for event in category_events:
             # Получение информации о каждом размещённом событии из кэша
-            event.update(get_or_set_event_cache(event['uuid'], 'event'))
+            event.update(get_or_set_event_cache(event['event_uuid'], 'event'))
+
+    # Получение событий во всех категориях или фильтр по конкретной категории
+    if slug == settings.BEZANTRAKTA_CATEGORY_ALL:
+        category_title = 'Все события'
+        category_events[:] = [ce for ce in category_events if ce['event_category_slug'] is not None]
+    else:
+        category_title = EventCategory.objects.values_list('title', flat=True).get(slug=slug)
+        category_events[:] = [ce for ce in category_events if ce['event_category_slug'] == slug]
 
     context = {
-        'title': category_name,
+        'title': category_title,
         'slug': slug,
         'category_events': category_events,
     }
