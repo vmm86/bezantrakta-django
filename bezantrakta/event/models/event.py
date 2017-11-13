eb9735b72dcbc89adcf63393a84e67d708a9018f
vmm <vmm86@users.noreply.github.com>
2017-11-13 21:16:28 +0000

    Коррективы вывода в __str__ экземпляра модели event.Event

--- bezantrakta/event/models/event.py
+++ bezantrakta/event/models/event.py
@@ -202,7 +202,7 @@ class Event(models.Model):
         )
 
     def __str__(self):
-        return '{event_or_group} {title} ({datetime}) {ts_event_id} - {domain}'.format(
+        return '({datetime}) {event_or_group} {ts_event_id} {title} - {domain}'.format(
             event_or_group='группа' if self.is_group else 'событие',
             title=self.title,
             ts_event_id=self.ticket_service_event if self.ticket_service_event is not None else '',
