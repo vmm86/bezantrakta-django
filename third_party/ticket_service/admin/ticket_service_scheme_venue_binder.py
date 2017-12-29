2912ec615e67828829a8f54b54ace136b68fb432
vmm <vmm86@users.noreply.github.com>
2017-12-29 13:31:31 +0000

    Правильное кэширование схем залов и секторов

--- third_party/ticket_service/admin/ticket_service_scheme_venue_binder.py
+++ third_party/ticket_service/admin/ticket_service_scheme_venue_binder.py
@@ -59,15 +59,24 @@ class TicketServiceSchemeVenueBinderAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def save_model(self, request, obj, form, change):
-        """Пересоздать кэш:
-        * при сохранении созданной ранее записи,
-        * если созданная ранее запись не пересохраняется в новую запись с новым первичным ключом.
-        """
-        super(TicketServiceSchemeVenueBinderAdmin, self).save_model(request, obj, form, change)
+        # Не сохранять экземпляр модели в методе по умолчанию,
+        # чтобы сначала сохранить все секторы, затем схему,
+        # и только после этого пересоздать кэш схемы зала
+        if not obj.pk:
+            super(TicketServiceSchemeVenueBinderAdmin, self).save_model(request, obj, form, change)
+        else:
+            pass
 
+    def save_formset(self, request, form, formset, change):
+        # Сохранить сначала схемы всех секторов в inline-формах
+        formset.save()
+        # Затем сохранить саму схему зала (обычную или посекторную)
+        form.instance.save()
+
+        # Пересоздать кэш схемы зала (с секторами или без)
         cache_factory(
-            'ticket_service_scheme', obj.ticket_service_scheme_id, reset=True,
-            ticket_service_id=obj.ticket_service_id
+            'ticket_service_scheme', form.instance.ticket_service_scheme_id, reset=True,
+            ticket_service_id=form.instance.ticket_service_id
         )
 
     def batch_set_cache(self, request, queryset):
