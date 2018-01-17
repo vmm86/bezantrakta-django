6f9d3f6ff2a82600075ae4e88276e87e56dba197
vmm <vmm86@users.noreply.github.com>
2018-01-17 15:34:50 +0000

    Методы 'total_plus_extra' и 'total_plus_courier_price' перемещены в класс TicketService

--- bezantrakta/order/views/order_info.py
+++ bezantrakta/order/views/order_info.py
@@ -10,6 +10,11 @@ def order_info(request, event, customer, order):
         event (TYPE): Информация о событии.
         order (TYPE): Информация о заказе.
     """
+    # Настройки сервиса продажи билетов
+    ticket_service = cache_factory('ticket_service', event['ticket_service_id'])
+    # Экземпляр класса сервиса продажи билетов
+    ts = ticket_service['instance']
+
     # Информация о сервисе онлайн-оплаты
     payment_service = cache_factory('payment_service', event['payment_service_id'])
     # Экземпляр класса сервиса онлайн-оплаты
@@ -21,12 +26,12 @@ def order_info(request, event, customer, order):
     # Получение общей суммы заказа в зависимости от возможных наценок/скидок
 
     # Для любого типа заказа - с учётом сервисного сбора для каждого билета в заказе (если он задан)
-    order['overall'] = ps.total_plus_extra(order['tickets'], order['total'], extra)
+    order['overall'] = ts.total_plus_extra(order['tickets'], order['total'], extra)
 
     # При доставке курьером - с учётом стоимости доставки курьером (если она задана)
     if customer['delivery'] == 'courier':
         # Общая сумма заказа (с учётом сервисного сбора и стоимости доставки курьером)
-        order['overall'] = ps.total_plus_courier_price(order['overall'], order['courier_price'])
+        order['overall'] = ts.total_plus_courier_price(order['overall'], order['courier_price'])
 
     # При онлайн-оплате - с учётом комиссии сервиса онлайн-оплаты (если она задана)
     if customer['payment'] == 'online':
