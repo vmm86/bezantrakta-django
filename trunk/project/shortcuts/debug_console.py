594ba0c7c660ad01542e669de0dc63ea552c0ca4
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2018-02-13 10:09:35 +0000

    Корретивы условного вывода информации в консоль с помощью debug_console

--- trunk/project/shortcuts/debug_console.py
+++ trunk/project/shortcuts/debug_console.py
@@ -2,12 +2,18 @@ from django.conf import settings
 
 
 def debug_console(*args):
-    """Вывод отладочной информации в консоль ТОЛЬКО в development-окружении.
-
-    Args:
-        *args: Description
-    """
+    """Вывод отладочной информации в консоль ТОЛЬКО при наличии булева параметра в настройках."""
     if settings.DEBUG and settings.BEZANTRAKTA_DEBUG_CONSOLE:
+        print('\033[1;93m', sep='', end='')
         for i in range(len(args)):
-            print('\033[1;93m', args[i], '\033[0;0m', sep='', end=' ')
-        print('\n')
+            print(args[i], sep='', end=' ')
+        print('\033[0;0m\n')
+
+# if level == 'INFO':
+#     self.stdout.write(self.style.WARNING(msg))
+# elif level == 'SUCCESS':
+#     self.stdout.write(self.style.SUCCESS(msg))
+# elif level == 'NOTICE':
+#     self.stdout.write(self.style.NOTICE(msg))
+# elif level == 'ERROR':
+#     self.stdout.write(self.style.ERROR(msg))
