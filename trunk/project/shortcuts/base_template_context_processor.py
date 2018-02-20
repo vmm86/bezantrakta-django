a7accbe1d9332e3b13b629ed42ed108499d5ac16
vmm <vmm@63076219-f2e4-45f0-9343-5e1fd119d72e>
2018-02-20 20:44:35 +0000

    Снятые с публикации сайты отображаются, если используется секретная cookie 🍪  для дебаггинга

--- trunk/project/shortcuts/base_template_context_processor.py
+++ trunk/project/shortcuts/base_template_context_processor.py
@@ -7,8 +7,12 @@ def base_template_context_processor(request):
     Returns:
         bool: Запускать процессор контекста или нет.
     """
+    # Разрешён ли вывод отладочной информации в консоли браузера
+    cookie_debugger = request.COOKIES.get(settings.BEZANTRAKTA_COOKIE_WATCHER_TITLE, None)
+    watcher = True if cookie_debugger == settings.BEZANTRAKTA_COOKIE_WATCHER_VALUE else False
+
     # Если сайт опубликован
-    domain_is_published = request.domain_is_published
+    domain_is_published = request.domain_is_published or watcher
 
     # Если пользователь НЕ в админ-панели
     frontend = settings.BEZANTRAKTA_ADMIN_URL not in request.url_path
